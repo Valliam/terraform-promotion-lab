01f5ad9d89bcc7a75399e0d4dc20b6780e9e55f5
valliam <valliamtune@gmail.com>
2026-08-31 17:58:29 +1000

    chore: add staging root module

---
+++ infra/envs/staging/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+variable "release_version" {
+  type = string
+}
+
+variable "forecast_interval_minutes" {
+  type = number
+}
+
+module "data_platform" {
+  source = "../../modules/data-platform"
+
+  environment               = "staging"
+  release_version           = var.release_version
+  forecast_interval_minutes = var.forecast_interval_minutes
+  worker_count              = 2
+}
+
+output "job_configuration" {
+  value = module.data_platform.job_configuration
+}
